afbe037239a003c7007700f5df7561c1c713d90e
Nikolas Andryuschenko <nikolaigeorgie@gmail.com>
2025-12-24 15:45:01 -0800

    Fix build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,27 +1,27 @@
-import NextAuth from "next-auth"
-import type { NextAuthOptions } from 'next-auth'
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { prisma } from "utils/prisma"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "utils/prisma";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    })
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // })
   ],
   callbacks: {
     async session({ session, token, user }) {
       if (session.user) {
-        session.user.id = user.id
+        session.user.id = user.id;
       }
 
-      return session
-    }
-  }
-}
+      return session;
+    },
+  },
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
